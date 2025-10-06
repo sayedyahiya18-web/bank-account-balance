0f724ef146a4b9ce3201bd66950f07404c760b88
sayedyahiya18-web <sayedyahiya18@gmail.com>
2025-10-06 17:43:19 +0530

    save the file

--- bank.py
+++ bank.py
@@ -1,8 +1,14 @@
 def update_balance():
     initial_balance = float(input("Enter initial balance: "))
     deposit = float(input("Enter deposit amount: "))
-
     updated_balance = initial_balance + deposit
-    print(f"Updated balance: {updated_balance}")
+    print(f"Balance after deposit: {updated_balance}")
+
+    withdrawal = float(input("Enter withdrawal amount: "))
+    if withdrawal > updated_balance:
+        print("Insufficient funds! Withdrawal denied.")
+    else:
+        updated_balance -= withdrawal
+        print(f"Balance after withdrawal: {updated_balance}")
 
 update_balance()
